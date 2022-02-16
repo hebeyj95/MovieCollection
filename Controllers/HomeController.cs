ae2b9bdc93f5c064f79a6b7927a6d17ca0e9fad2
hebeyj95 <62028714+hebeyj95@users.noreply.github.com>
2022-02-15 21:01:19 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using BaconsaleMovieCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaconsaleMovieCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MoviesContext moviesContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, MoviesContext movies)
+        {
+            _logger = logger;
+            moviesContext = movies;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddMovie()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddMovie(ApplicationResponse model)
+        {
+            moviesContext.Add(model);
+            moviesContext.SaveChanges();
+            return View("ConfirmationPage", model);
+        }
+
+        public IActionResult Baconsale()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MoviesContext.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BaconsaleMovieCollection.Models
+{
+    public class MoviesContext : DbContext
+    {
+        //Constructor
+        public MoviesContext(DbContextOptions<MoviesContext> options) : base(options)
+        {
+            //leave blank for now
+        }
+        public DbSet<ApplicationResponse> responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<ApplicationResponse>().HasData(
+                new ApplicationResponse
+                {
+                    ApplicationId = 1,
+                    Category = "Comedy",
+                    Title = "Hitch",
+                    Year = 2005,
+                    Director = "Andy Tennant",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
+                },
+                new ApplicationResponse
+                {
+                    ApplicationId = 2,
+                    Category = "Family",
+                    Title = "Toy Story",
+                    Year = 1995,
+                    Director = "John Lasseter",
+                    Rating = "G",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
+                },
+                new ApplicationResponse
+                {
+                    ApplicationId = 3,
+                    Category = "Action/Adventure",
+                    Title = "Spider-Man",
+                    Year = 2002,
+                    Director = "Sam Raimi",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = "",
+                }
+            );
+        }
+    }
+}
